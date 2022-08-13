1b9e9742cf6b92ada8291ea6ccf4a17ddbbf8c24
Makhach Abdulazizov <abdulazizov2001@mail.ru>
2022-08-13 20:26:34 +0300

    basic pointer learn

--- main.cpp
+++ main.cpp
@@ -1,4 +1,23 @@
+#include <iostream>
 
 int main() {
+    int a = 12;
+    int &refA = a;
+    std::cout << "Ссылка до изменения:  " << &refA << std::endl;
+    std::cout << "Значение по ссылке: " << refA << std::endl;
+    refA = 20;
+    std::cout << "Ссылка после изменения: " << &refA << std::endl;
+    std::cout << "Значение по ссылке: " << refA << std::endl;
+    int b = a;
+    b = 40;
+    std::cout << "Ссылка копии A: " << &b << std::endl;
+    std::cout << "Ссылка А: " << &refA << std::endl;
+    std::cout << "Значение копии A: " << b << std::endl;
+    std::cout << "Значение A: " << a << std::endl;
+    int *pointerA = &a;
+    *pointerA = 23;
+    std::cout << "Указатель A: " << &pointerA << std::endl;
+    std::cout << "Значение указателя A: " << *pointerA << std::endl;
+    std::cout << "Значение A: " << a << std::endl;
     return 0;
 }
