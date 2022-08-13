a3933056146dc3fd57d5c5fa7ef79e0e634a945c
Makhach Abdulazizov <abdulazizov2001@mail.ru>
2022-08-13 20:36:57 +0300

    transfer var by link to functions

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void calc(int num1, int num2, int *sum);
 
 int main() {
+    int num1 = 5;
+    int num2 = 10;
+    int sum = 0;
+    calc(num1, num2, &sum);
+    cout << "Значение после суммы: " << sum << endl;
     return 0;
+}
+
+void calc(int num1, int num2, int *sum) {
+    *sum = num1 + num2;
 }
