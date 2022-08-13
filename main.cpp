26f288b51d0325e46435f96f265eecaf6649b534
Makhach Abdulazizov <abdulazizov2001@mail.ru>
2022-08-13 20:38:47 +0300

    work with arrays as param to function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void changeFirstElementOnArray(int *_array);
 
 int main() {
+    int array1[] = {1, 2, 3, 4, 5};
+
+    changeFirstElementOnArray(array1);
+    cout << "Значение после изменения первого элемента: " << array1[0] << endl;
     return 0;
+}
+
+void changeFirstElementOnArray(int *array) {
+    array[0] = 999;
 }
