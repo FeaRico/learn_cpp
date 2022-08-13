b0753ec2500ad1bec2a72afbfb82f434a0bac1c3
Makhach Abdulazizov <abdulazizov2001@mail.ru>
2022-08-13 20:30:53 +0300

    basic operations with pointers

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+
+using namespace std;
 
 int main() {
+    int d = 15;
+    int *pointerD = &d;
+    cout << "До инкремента указателя d (ссылка): " << pointerD << endl;
+    cout << "До инкремента указателя d (значение): " << *pointerD << endl;
+    pointerD++;
+    cout << "После инкремента указателя d (ссылка): " << pointerD << endl;
+    cout << "После инкремента указателя d (значение): " << *pointerD << endl;
+
+    cout << "До декремента указателя d (ссылка): " << pointerD << endl;
+    cout << "До декремента указателя d (значение): " << *pointerD << endl;
+    pointerD--;
+    cout << "После декремента указателя d (ссылка): " << pointerD << endl;
+    cout << "После декремента указателя d (значение): " << *pointerD << endl;
     return 0;
 }
