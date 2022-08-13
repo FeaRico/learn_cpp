28669ae884858185b3642b75ed672695dca42036
Makhach Abdulazizov <abdulazizov2001@mail.ru>
2022-08-13 20:27:46 +0300

    char pointer

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+
+using namespace std;
 
 int main() {
+    char c = '*';
+    cout << "Символ: " << c << endl;
+    char &refC = c;
+    cout << "Ссылка на символ: " << refC << endl;
+    char *pointerC = &c;
+    cout << "Указатель на символ: " << pointerC << endl;
+    int *intPointerC = (int *) &c;
+    cout << "Указатель на символ (int): " << intPointerC << endl;
     return 0;
 }
