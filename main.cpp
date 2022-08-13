ba5e7b2b0095a46c5cc678e89dde077db78226e7
Makhach Abdulazizov <abdulazizov2001@mail.ru>
2022-08-13 20:34:32 +0300

    learn work pointer with arrays

--- main.cpp
+++ main.cpp
@@ -1,4 +1,24 @@
+#include <iostream>
+
+using namespace std;
 
 int main() {
+    int array[] = {1, 2, 3, 4, 5};
+    cout << "Первый элемент (ссылка): " << array << endl;
+    cout << "Первый элемент (значение): " << *array << endl;
+    int *twoElementPtr = array + 1;
+    cout << "Второй элемент (ссылка):" << twoElementPtr << endl;
+    cout << "Второй элемент (значение):" << *twoElementPtr << endl;
+    int *threeElementPtr = array + 2;
+    cout << "Третий элемент (ссылка):" << threeElementPtr << endl;
+    cout << "Третий элемент (значение):" << *threeElementPtr << endl;
+
+    cout << "Перебор элементов массива" << endl;
+
+    for (int *ptr = array, i = 1; ptr <= &array[4]; ptr++) {
+        cout << "Ссылка " << i << " элемента: " << ptr << endl;
+        cout << "Значение " << i << " элемента: " << *ptr << endl;
+        i++;
+    }
     return 0;
 }
